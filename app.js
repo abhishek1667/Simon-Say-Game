8bbdd5b31cb1b6098e223e8369b2433fdbdcd5b5
abhishek1667 <136836702+abhishek1667@users.noreply.github.com>
2023-08-07 21:42:37 +0530

    app.js

--- app.js
+++ app.js
@@ -40,12 +40,13 @@ function levelUp() {
 
 function checkAns(idx) {
 
-    if(userSeq[idx] === gameSeq[idx] && userSeq.length == gameSeq.length) {
+    if(userSeq[idx] === gameSeq[idx]) {
+        if(userSeq.length == gameSeq.length)
             setTimeout(levelUp, 1000);
     } else {
         highestScore.push(level);
         highestScore.sort((a, b) => a - b);
-        p.innerHTML = `Game over! Your score was <b>${highestScore[highestScore.length - 1]}<b/><br>Press any key to start.`;
+        p.innerHTML = `Game over! Your highest score is <b>${highestScore[highestScore.length - 1]}<b/><br>Press any key to start.`;
         document.querySelector("body").style.backgroundColor = "red";
         setTimeout(function () {
             document.querySelector("body").style.backgroundColor = "transparent";
